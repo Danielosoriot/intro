4f7eb5a3c9ca14c8283996b367e1a3d3767ab637
Danielosoriot <danielosoriot64@gmail.com>
2025-08-28 09:58:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
-
+from PIL import Image
 
 st.title("Tralarero")
+
+st.header("BRR BRR PATAPIN TRALARERO TRALALA")
+st.write("Anuel la Doble AA ")
+image = Image.open("trala.png")
+
+st.image(image, caption="trala")
